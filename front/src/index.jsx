4090b9c00243f11a7763104bd2a3fceb53471f80
sylvain gourier <sylvain.gourier@gmail.com>
2022-06-24 10:05:31 +0200

    Upd: Files positions

---
+++ front/src/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Header from './components/Header'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    />
+
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    />
+
+    <Header />
+  </React.StrictMode>
+);
